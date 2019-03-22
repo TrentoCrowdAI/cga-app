83fe284fd85a726cd472f6a8b8a16e5ebdbac0ae
Giovanni Guadagnini <giovanni.guadagnini@gmail.com>
2019-03-22 12:15:15 +0100

    Added timer

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
-import NavigatorButton from "./src/NavigatorButton";
-import MyTimer from "./src/MyTimer";
-import Question from "./src/Question"
+import MainUI from "./src/MainUI";
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.rootContainer}>
-        <View style={styles.timerContainer}>
-          <MyTimer />
-        </View>
-        <View style={styles.questionContainer}>
-          <Question />
-        </View>
-        <View style={styles.navigatorButtonContainer}>
-          <NavigatorButton />
-        </View>
+        <MainUI />
       </View>
     );
   }
@@ -26,14 +16,5 @@ const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
     flexDirection: 'column'
-  },
-  timerContainer: {
-    flex: 1,
-  },
-  questionContainer: {
-    flex: 5
-  },
-  navigatorButtonContainer: {
-    flex: 1
   }
 });
